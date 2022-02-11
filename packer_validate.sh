5dfea4af705405c2e0a2e70307c73b1ccd215447
Imran Asif <imran@pop-os.localdomain>
2022-02-11 11:57:10 -0700

    updated the validation script.

--- packer_validate.sh
+++ packer_validate.sh
@@ -2,4 +2,13 @@
 
 set -e
 
+EXIT_CODE=0
+
+[-d "$1"] || (EXIT_CODE=$? && echo "Directory does not exist. Exiting with error." && exit $EXIT_CODE)
+
+echo "Checking packer config formatting..."
 packer fmt -check -recursive "$1"/
+
+echo "Running syntax validtion for the provided repo."
+for file in $(find $1 -iname "*.pkr.hcl"); do
+    echo $file
