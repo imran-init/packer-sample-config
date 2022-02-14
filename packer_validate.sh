8f3484648e1b4c6f1da25a6ae9f59c7fa7959d26
Imran Asif <imran@pop-os.localdomain>
2022-02-14 12:46:59 -0700

    Updated the bash to print the ENV.

--- packer_validate.sh
+++ packer_validate.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+echo "Echoing ENV Vars."
+echo "FILES: $FILES"
+echo "ACTION: $ACTION"
+
 EXIT_CODE=0
 
 [ -d "$1" ] || (EXIT_CODE=$? && echo "Directory does not exist. Exiting with error." && exit $EXIT_CODE)
 
 echo "Checking packer config formatting..."
-packer fmt -check -recursive "$1"/
+packer fmt -check -recursive "$1"
 
 
 
