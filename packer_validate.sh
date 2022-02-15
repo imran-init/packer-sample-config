f70b27ed1d69b0267a9a07b3b8618b64a9d647c9
Imran Asif <imran@pop-os.localdomain>
2022-02-15 10:44:29 -0700

    Used if else.

--- packer_validate.sh
+++ packer_validate.sh
@@ -12,8 +12,8 @@ if [[ "${ACTION}" == "fmt" || "${ACTION}" == "" ]]; then
     echo "fmt was sent"
     FMT_ERROR=0
     for dir in $(echo "$FILES" | xargs -n1 dirname | sort -u | uniq); do
-    echo "--> Running 'packer fmt -check -recursive' in directory '$dir'"
-    pushd "${dir}" >/dev/null
+    echo "--> Running 'packer fmt -check -recursive' in directory 'repository/${dir}'"
+    pushd "repository/${dir}" >/dev/null
     packer fmt -check -recursive . || FMT_ERROR=$?
     popd >/dev/null
     done
@@ -24,7 +24,7 @@ if [[ "${ACTION}" == "validate" || "${ACTION}" == "" ]]; then
     echo "validate was sent"
     VALIDATE_ERROR=0
     for dir in $(echo "$FILES" | xargs -n1 dirname | sort -u | uniq); do
-    echo "--> Running 'packer validate -syntac-only' in directory '$dir'"
+    echo "--> Running 'packer validate -syntac-only' in directory 'repository/${dir}'"
     pushd "repository/${dir}" >/dev/null
     packer validate -syntax-only . || VALIDATE_ERROR=$?
     popd >/dev/null
