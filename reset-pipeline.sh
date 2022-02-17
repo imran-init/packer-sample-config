2d7b8007ca6ade8bc9a931a53a8e8c570d39cd2c
Imran Asif <imran@pop-os.localdomain>
2022-02-16 17:14:02 -0700

    Updated

---
+++ reset-pipeline.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fly -t ps destroy-pipeline -p pipeline_packer.yml -n
+fly -t ps set-pipeline -c pipeline_packer.yml -p pipeline_packer.yml -n
+fly -t ps unpause-pipeline -p pipeline_packer.yml
+fly -t ps trigger-job -j pipeline_packer.yml/unit-test
